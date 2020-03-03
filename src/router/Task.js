7a54070dc6e6cdf8a08552300a661490170b79f3
unknown <nikhil19@somaiya.edu>
2020-03-03 23:42:49 +0530

    finish sorting filtering and pagination

--- src/router/Task.js
+++ src/router/Task.js
@@ -17,10 +17,29 @@ router.post("/tasks", auth, async (req, res) => {
     res.status(400).send(e);
   }
 });
-
+//GET tasks?completed=
+//GET /tasks?limit=10&skip=10
+//GET /tasks?sortBy=createdAt:asc
 router.get("/tasks", auth, async (req, res) => {
+  const match = {}
+  const sort = {}
+  if(req.query.completed){
+    match.completed = req.query.completed==="true"
+  }
+  if(req.query.sortBy){
+    const parts = req.query.sortBy.split(":")
+    sort[parts[0]]=parts[1]==="desc" ? -1 :1
+  }
   try {
-    await req.user.populate("tasks").execPopulate();
+    await req.user.populate({
+      path:"tasks",
+      match,
+      options:{
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort
+      }
+    }).execPopulate();
     res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send();
@@ -71,10 +90,13 @@ router.patch("/tasks/:id", auth, async (req, res) => {
   }
 });
 
-router.delete("/tasks/:id", async (req, res) => {
-  const task = Task.findByIdAndDelete(req.params.id);
+router.delete("/tasks/:id", auth, async (req, res) => {
+  const task = await Task.findOneAndDelete({
+    _id: req.params.id,
+    owner: req.user._id
+  });
   try {
-    if (!user) {
+    if (!task) {
       res.status(404).send();
     }
     res.send(task);
